d18250071494b09fc875f6cb973153403b336d9a
Marius-George Olaru <MariusOl@yonder.local>
2019-03-25 19:32:03 +0200

    Added package

--- src/main/java/com/example/pcd/controllers/HomeController.java
+++ src/main/java/com/example/pcd/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.pcd;
+package com.example.pcd.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
